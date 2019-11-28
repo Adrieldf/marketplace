401064a54ec0281699b811c66c137146e3a09e92
Adriel <adriel.idf@gmail.com>
2019-11-27 21:21:17 -0300

    fix

--- html/buscaProdutos.php
+++ html/buscaProdutos.php
@@ -11,33 +11,25 @@ $dao = new ProdutoDAO();
 $produtos = $dao->listarTodos();
 
 ?>
-<section class="panel col-lg-9">
-
-    <header class="panel-heading">
-        Dados da busca:
-    </header>
     <?php
     if(sizeof($produtos)>0){
     ?>
-    <table class="table table-striped table-advance table-hover">
-        <tbody>
-            <tr>
-                <th><i class="icon_profile"></i> Nome</th>
-                <th><i class="icon_profile"></i> Modelo</th>
-                <th><i class="icon_mail_alt"></i> Descrioção</th>
-            </tr>
-            <?php 
-            foreach ($produtos as $produto) {
-            ?>
-            <tr>
-                <td><?=$produto->getNome();?></td>
-                <td><?=$produto->getModelo();?></td>
-                <td><?=$produto->getDescricao();?></td>
-            </tr>
+    <?php 
+    foreach ($produtos as $produto) {
+    ?>
+    	<article class="item">
+					<header>
+						<a href="../html/detalheProdutos.php"><img src="https://images-submarino.b2w.io/produtos/01/00/sku/7929/7/7929783_1GG.jpg" /></a>
+                        <h3><?=$produto->getNome();?> </h3>
+					</header>
+					<p><?=$produto->getModelo();?></p>
+					<p><?=$produto->getDescricao();?></p>
+					<ul class="actions">
+                        <h2>R$ <?=$produto->getPreco();?></h2>
+					</ul>
+        </article>
             <?php }?>
-        </tbody>
-    </table>
+     
     <?php }else{?>
     <h4>Nao foram encontrados registros com esta palavra.</h4>
     <?php }?>
-</section>
--- html/galeriaProdutos.php
+++ html/galeriaProdutos.php
@@ -12,26 +12,8 @@
 	<?PHP include_once("navbar.html"); ?>
 	<button id="buscar"  >Buscar</button>
 	<section id="listaProdutos" class="main items">
-		<article class="item">
-							<header>
-								<a href="../html/detalheProdutos.php"><img src="https://images-submarino.b2w.io/produtos/01/00/sku/7929/7/7929783_1GG.jpg" /></a>
-                                <h3>Suporte para TV </h3>
-							</header>
-							<p>Suporte para TV Fixo de Parede Genius de 14 à 84" - ELG Suportes</p>
-							<ul class="actions">
-                                <h2>R$14,89</h2>
-							</ul>
-		</article>
-		<article class="item">
-							<header>
-								<a href="#"><img src="https://images-submarino.b2w.io/produtos/01/00/item/123187/4/123187493_1GG.jpg" alt="" /></a>
-								<h3>Antena Digital Aquário Indoor DTV-1000</h3>
-							</header>
-							<p>A antena de Tv Interna da Aquário DTV ¿ 100 apresenta excelente qualidade, funcionalidade..</p>
-							<ul class="actions">
-								<h2>R$21,00</h2>
-							</ul>
-		</article>
+	
+		
 	</section>
 			</div>
 
